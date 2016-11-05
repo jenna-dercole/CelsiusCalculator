bfbc253c014e6be0f3084d7eeaf3dd4cd8a5d347
Genna1 <Genna1@192.168.2.95>
2016-11-05 12:04:37 -0500

    Completed fahrenheit to celsius calculator (no validation yet).

---
+++ src/celsiuscalculator/CelsiusCalculator.java
@@ -0,0 +1,33 @@
+
+package celsiuscalculator;
+
+/**
+ *
+ * @author Jenna
+ */
+public class CelsiusCalculator {
+    private double celsiusTemp;
+    private double fahrenheitTemp;
+
+    public final double getFahrenheitTemp() {
+        return fahrenheitTemp;
+    }
+
+    public final void setFahrenheitTemp(double fahrenheitTemp) {
+        this.fahrenheitTemp = fahrenheitTemp;
+    }
+
+    public final double getCelsiusTemp() {
+        return celsiusTemp;
+    }
+
+    public final void setCelsiusTemp(double celsiusTemp) {
+        this.celsiusTemp = celsiusTemp;
+    }
+    
+    public final double calculateCelsius(double fahrenheit) {
+        celsiusTemp = (fahrenheit - 32) * 5 / 9;
+        return celsiusTemp;
+    }
+   
+}
